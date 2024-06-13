2a8b3d2256e1fd1ce9a897c2b0a67c5ccbb13c72
이태민 <itaemin@itaemins-MacBook-Pro.local>
2024-06-13 18:20:01 +0900

    Done with excercise from 1 ~ 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(reversed) {
+    let revString = '';
+    for (let i = reversed.length-1; i >= 0; i--){
+        revString += reversed[i];
+    }
+    return revString;
 };
 
 // Do not edit below this line
