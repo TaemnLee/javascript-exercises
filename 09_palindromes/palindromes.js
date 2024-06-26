92016ce443b0fe54140ee1e738a8652b79eb39cd
이태민 <itaemin@itaemins-MacBook-Pro.local>
2024-06-26 15:43:02 +0900

    Completed the foundation javascript excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    // Step 1: Transform the string to lowercase, filter only alphanumeric characters, and rejoin as a new string
+    const filteredStr = str.toLowerCase()
+                         .split('')
+                         .filter((character) => alphanumerical.includes(character))
+                         .join('');
+
+    // Step 2: Check if the filtered string is a palindrome
+    let isPal = true;
+    let i = 0;
+    while (isPal && i < Math.floor(filteredStr.length / 2)) {
+        if (filteredStr[i] !== filteredStr[filteredStr.length - 1 - i]) {
+            isPal = false;
+        }
+        i++;
+    }
+
+    // Step 3: Return the result
+    return isPal;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fib
+    if (n != "number"){
+        fib = parseInt(n);
+    } else {
+        fib = n;
+    }
 
+    if (fib < 0) return "OOPS";
+
+    if (fib === 0) return 0;
+
+    let prev = 1;
+    let prevprev = 0;
+
+    for (let i = 2; i <= fib; i++){
+        let current = prev + prevprev;
+        prevprev = prev;
+        prev = current;
+    }
+    return prev;
 };
 
 // Do not edit below this line
